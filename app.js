2cee066016d8ec2b9ff5fe17e6675a1916277001
kareemah <ajimobikareemah@gmail.com>
2023-03-08 11:23:36 +0700

    add security packages, error handling left

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ const connectDB = require('./db/connect');
 const notFoundMiddleware = require('./middleware/notfound');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authMiddleware = require('./middleware/auth');
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimiter = require();
+// const
 
 app.use(express.json());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authMiddleware, jobRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+app.use(cors);
+app.use(helmet);
+app.use(xss);
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 const start = async () => {
